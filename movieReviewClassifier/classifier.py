ae3b799c66362d826eda11b2f67a76a697097add
dhruv-karan <dhruvkaran24@gmail.com>
2019-02-03 22:32:52 +0530

    DEFINED THE MODEL

--- movieReviewClassifier/classifier.py
+++ movieReviewClassifier/classifier.py
@@ -11,3 +11,33 @@ reverse_word_index = dict([(value,key) for (key,value) in word_index.items()])
 
 decoded_review = ' '.join([reverse_word_index.get(i-3,'?') for i in train_images[0]])
 print(reverse_word_index.get(10))
+
+#=========ENCODING THE INTEGER MATRIX INTO BINARY MATRIX=========================
+import numpy as np
+def vectorise_sequence(sequence,dimension=10000):
+    result = np.zeros((len(sequence),dimension))
+    for i, sequen in enumerate(sequence):
+        result[i,sequen] = 1
+    return result
+
+train_images = vectorise_sequence(train_images)
+test_images = vectorise_sequence(test_images)
+
+y_train = np.asarray(train_labels).astype('float32')
+y_test = np.asarray(test_labels).astype('float32')
+
+#============================ DEFINING MODEL===========================
+from keras import layers,models
+
+model = models.Sequential()
+model.add(layers.Dense(16,activation='relu',input_shape=(10000,)))
+model.add(layers.Dense(16,activation='relu'))
+model.add(layers.Dense(1,activation='sigmoid'))
+
+#-=====================----COMPLILING THE MODEL=================
+
+model.compile(optimizer='rmsprop',
+              loss='binary_crossentropy',
+              metrics=['accuracy'])
+
+#======================MAKIN
